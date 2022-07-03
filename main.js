4c8c8d68716e551a03d2a29c10a3b7af58650c21
Lea <odinns_world@hotmail.com>
2022-07-03 14:42:44 +0100

    Why are my commits not being added to accoutn?

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // My daily Codewars answers. Newest first.
 
 // Counting sheep... 8kyu
-// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present)
 function countSheeps(arrayOfSheep) {
     let totalSheep = 0;
     arrayOfSheep.forEach(sheep => sheep ? totalSheep++ : 0)
