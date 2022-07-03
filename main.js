df71072bf8596d879a758f13576f49fe23792cca
unknown <LeaBrown198x@gmail.com>
2022-07-03 16:37:58 +0100

    Sorting out account

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function countSheeps(arrayOfSheep) {
 }
 
 // Summation 8kyu
-// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0
 let summation = function (num) {
     let total = 0;
     for (let i = 1; i <= num; i++) {
