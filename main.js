37eccfcd7e38a903207f276222008f2be29d3242
Lea <odinns_world@hotmail.com>
2022-07-03 14:29:03 +0100

    Codewars 3/7

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-// My daily Codewars answers
+// My daily Codewars answers. Newest first.
+
+// Counting sheep... 8kyu
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+function countSheeps(arrayOfSheep) {
+    let totalSheep = 0;
+    arrayOfSheep.forEach(sheep => sheep ? totalSheep++ : 0)
+    return totalSheep;
+}
 
 // Summation 8kyu
 // Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
