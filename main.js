d964688e32aac0341d2f168a1f5b51ef067aeaed
Lea Brown <LeaBrown198x@gmail.com>
2022-08-04 13:56:45 +0100

    codewars 4/8

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 // My daily Codewars answers. Newest first.
 
+/* Find the odd int 6kyu
+Googled ways to find odd elements in array and stumbled on this solution. I don't really understand it. At the moment anyway.
+https://www.codewars.com/kata/54da5a58ea159efa38000836/javascript */
+function findOdd(A) {
+    return A.reduce((a, c) => a ^ c, 0);
+}
+
 /* Array.diff 6kyu
 https://www.codewars.com/kata/523f5d21c841566fde000009/javascript */
 function arrayDiff(a, b) {
