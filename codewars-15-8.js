c166b6c454a8a5a21fe4835ec79ec60d43cf9d41
Lea Brown <LeaBrown198x@gmail.com>
2022-08-15 19:16:19 +0100

    codewars 15/8

---
+++ codewars-15-8.js
@@ -0,0 +1,28 @@
+/* Mirror object - tcejbo rorriM 6kyu
+
+https://www.codewars.com/kata/586305e8916e244b66001a93/javascript
+
+Mirror - Mirror
+Can you mirror the properties on an object?
+
+Given an object with properties with no value
+
+abc: -
+arara: -
+xyz: -
+Return a new object that have the properties with its mirrored key!
+
+abc: cba
+arara: arara
+xyz: zyx
+
+"You cannot change the original object, because if you did that the reflection would change."
+*/
+
+const mirror = obj => {
+    let newObj = {}
+    Object.keys(obj).forEach(x => {
+        newObj[`${x}`] = x.split('').reverse().join('')
+    })
+    return newObj;
+};
