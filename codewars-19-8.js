480bc5596096e52e624d4d8df2d8ead195281c1e
Lea Brown <LeaBrown198x@gmail.com>
2022-08-19 22:59:08 +0100

    evening codewars

--- codewars-19-8.js
+++ codewars-19-8.js
@@ -16,3 +16,27 @@ function count(string) {
     })
     return result;
 }
+
+/* Mumbling 7kyu
+
+https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/javascript
+
+DESCRIPTION:
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
+
+function accum(s) {
+    let newStr = "";
+    s.split('')
+    for (let i = 0; i < s.length; i++) {
+        newStr += `${s[i].toUpperCase()}${s[i].toLowerCase().repeat(i)}-`
+    }
+    return newStr.slice(0, -1);
+}
