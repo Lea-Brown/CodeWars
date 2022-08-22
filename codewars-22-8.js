6da9c13068e8f55b11650b7729b5ded45f01e848
Lea Brown <LeaBrown198x@gmail.com>
2022-08-22 13:12:18 +0100

    more codewars

--- codewars-22-8.js
+++ codewars-22-8.js
@@ -56,3 +56,30 @@ function greet(language) {
         return langObj[language]
     }
 }
+
+/* Count the divisors of a number 7kyu
+
+https://www.codewars.com/kata/542c0f198e077084c0000c2e/javascript
+
+DESCRIPTION:
+Count the number of divisors of a positive integer n.
+
+Random tests go up to n = 500000.
+
+Examples (input --> output)
+4 --> 3 (1, 2, 4)
+5 --> 2 (1, 5)
+12 --> 6 (1, 2, 3, 4, 6, 12)
+30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+*/
+
+function getDivisorsCnt(n) {
+    console.log(n)
+    let result = 0;
+    for (let i = n; i >= 0; i--) {
+        if (n % i === 0) {
+            result++
+        }
+    }
+    return result;
+}
