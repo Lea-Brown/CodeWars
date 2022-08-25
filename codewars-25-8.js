e25802f43d0bf10dcebab8e1592c07cbd905729b
Lea Brown <LeaBrown198x@gmail.com>
2022-08-25 14:21:30 +0100

    more wars

--- codewars-25-8.js
+++ codewars-25-8.js
@@ -51,3 +51,21 @@ const quarterOf = (month) => {
             return "Not between 1 and 12"
     }
 }
+
+/* 8 kyu
+Removing Elements
+
+https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/javascript
+
+DESCRIPTION:
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+*/
+
+function removeEveryOther(arr) {
+    return arr.filter((e, i) => i % 2 === 0);
+}
