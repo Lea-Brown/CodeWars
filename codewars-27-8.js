d7f1306b095c4382f2dc9b3ffbdb7e077f7c740d
Lea Brown <LeaBrown198x@gmail.com>
2022-08-27 17:45:37 +0100

    more wars++

--- codewars-27-8.js
+++ codewars-27-8.js
@@ -1,3 +1,28 @@
+/* 6 kyu
+Persistent Bugger.
+
+https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/javascript
+
+DESCRIPTION:
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+4 --> 0 (because 4 is already a one-digit number)
+*/
+
+function persistence(num) {
+    let result = num.toString();
+    let i = 0
+    while (result.length > 1) {
+        result = result.split('').reduce((acc, x) => acc * x, 1).toString();
+        i++;
+    }
+    return i;
+}
+
 /* 7 kyu
 Find the next perfect square!
 
@@ -52,4 +77,36 @@ printer_error(s) => "8/22"
 function printerError(s) {
     let errors = s.match(/[^abcdefghijklm]/g) || [];
     return `${errors.length}/${s.length}`;
+}
+
+/* 7 kyu
+Binary Pyramid 101
+
+https://www.codewars.com/kata/5596700a386158e3aa000011/javascript
+
+DESCRIPTION:
+Given two numbers m and n, such that 0 ≤ m ≤ n :
+
+convert all numbers from m to n (inclusive) to binary
+sum them as if they were in base 10
+convert the result to binary
+return as a string
+Example
+1, 4  -->  1111010
+
+because:
+    1  // 1 in binary is 1
++  10  // 2 in binary is 10
++  11  // 3 in binary is 11
++ 100  // 4 in binary is 100
+-----
+  122  // 122 in binary is 1111010
+*/
+
+function binaryPyramid(m, n) {
+    let result = 0;
+    for (let i = m; i <= n; i++) {
+        result += Number(i.toString(2))
+    }
+    return result.toString(2);
 }
