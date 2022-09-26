1f53276ebfebf121290ad01470c4bf55ef3e4712
Lea Brown <LeaBrown198x@gmail.com>
2022-09-26 14:52:53 +0100

    codewars 26/9

--- 7kyu/Remove-the-minimum.js
+++ 7kyu/Remove-the-minimum.js
@@ -1,39 +1,5 @@
-/* A Needle in the Haystack 8kyu
-
-https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/javascript
-
-DESCRIPTION:
-Can you find the needle in the haystack?
-
-Write a function findNeedle() that takes an array full of junk but containing one "needle"
-
-After your function finds the needle it should return a message (as a string) that says:
-
-"found the needle at position " plus the index it found the needle, so:
-
-Example(Input --> Output)
-
-["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
-Note: In COBOL, it should return "found the needle at position 6"
-*/
-
-function findNeedle(haystack) {
-    let needle;
-    haystack.forEach((x, i) => {
-        if (x === 'needle') {
-            needle = i
-        }
-    })
-    return `found the needle at position ${needle}`
-}
-
-// refactor
-
-function findNeedle(haystack) {
-    return `found the needle at position ${haystack.indexOf('needle')}`
-}
-
-/* Remove the minimum 7kyu
+/* 7kyu
+Remove the minimum 
 
 https://www.codewars.com/kata/563cf89eb4747c5fb100001b/javascript
 
---
+++ 8kyu/A-Needle-in-the-Haystack.js
@@ -0,0 +1,35 @@
+/* 8kyu
+A Needle in the Haystack
+
+https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/javascript
+
+DESCRIPTION:
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+Note: In COBOL, it should return "found the needle at position 6"
+*/
+
+function findNeedle(haystack) {
+    let needle;
+    haystack.forEach((x, i) => {
+        if (x === 'needle') {
+            needle = i
+        }
+    })
+    return `found the needle at position ${needle}`
+}
+
+// refactor
+
+function findNeedle(haystack) {
+    return `found the needle at position ${haystack.indexOf('needle')}`
+}
