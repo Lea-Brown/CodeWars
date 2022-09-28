0d9a277c4baee4ad589c26c56c3054f615c4a6b2
Lea Brown <LeaBrown198x@gmail.com>
2022-09-28 16:12:47 +0100

    codewars 28/9

---
+++ 7kyu/Sort-Numbers.js
@@ -0,0 +1,18 @@
+/* 7 kyu
+Sort Numbers
+
+https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/javascript
+
+DESCRIPTION:
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+*/
+
+function solution(nums) {
+    if (nums == null || nums.length === 0) return [];
+    return nums.sort((a, b) => a - b);
+}
