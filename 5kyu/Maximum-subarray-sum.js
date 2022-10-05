882c20c777182f9f0e2518765c1562d47b51c48a
Lea Brown <LeaBrown198x@gmail.com>
2022-10-05 18:33:42 +0100

    codewars 5/10

---
+++ 5kyu/Maximum-subarray-sum.js
@@ -0,0 +1,26 @@
+/* 5 kyu
+Maximum subarray sum
+
+https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/javascript
+
+DESCRIPTION:
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+// should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+*/
+
+var maxSequence = function (arr) {
+    if (arr.length == 0 || arr.every(x => x < 0)) return 0;
+
+    let count = 0;
+    for (let a = 0; a <= arr.length; a++) {
+        for (let i = a + 1; i <= arr.length; i++) {
+            let sum = arr.slice(a, i).reduce((a, b) => a + b);
+            if (sum > count) { count = sum }
+        }
+    } return count;
+}
---
+++ 8kyu/Quarter-of-the-year.js
@@ -0,0 +1,33 @@
+/* 8 kyu
+Quarter of the year
+
+https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/javascript
+
+DESCRIPTION:
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+*/
+
+const quarterOf = (month) => {
+    switch (month) {
+        case 1:
+        case 2:
+        case 3:
+            return 1;
+        case 4:
+        case 5:
+        case 6:
+            return 2;
+        case 7:
+        case 8:
+        case 9:
+            return 3;
+        case 10:
+        case 11:
+        case 12:
+            return 4;
+        default:
+            return "Not between 1 and 12"
+    }
+}
