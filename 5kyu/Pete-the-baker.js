977065d330679949e0bda05046cac676680491a7
Lea Brown <LeaBrown198x@gmail.com>
2022-10-07 12:28:59 +0100

    codewars 7/10

---
+++ 5kyu/Pete-the-baker.js
@@ -0,0 +1,34 @@
+/* 5 kyu
+Pete, the baker
+
+https://www.codewars.com/kata/525c65e51bf619685c000059/javascript
+
+DESCRIPTION:
+Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+
+Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
+
+Examples:
+
+// must return 2
+cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
+// must return 0
+cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk
+*/
+
+function cakes(recipe, available) {
+    let result = 0;
+    for (let x in recipe) {
+        let count = Math.floor(available[x] / recipe[x]);
+        if (isNaN(count)) {
+            return 0;
+        } else if (count == 0) {
+            return 0;
+        } else if (result == 0) {
+            result = count;
+        } else if (count < result) {
+            result = count;
+        }
+    }
+    return result;
+}
---
+++ 7kyu/Binary-Pyramid-101.js
@@ -0,0 +1,31 @@
+/* 7 kyu
+Binary Pyramid 101
+
+https://www.codewars.com/kata/5596700a386158e3aa000011/javascript
+
+DESCRIPTION:
+Given two numbers m and n, such that 0 ≤ m ≤ n :
+
+convert all numbers from m to n (inclusive) to binary
+sum them as if they were in base 10
+convert the result to binary
+return as a string
+Example
+1, 4  -->  1111010
+
+because:
+    1  // 1 in binary is 1
++  10  // 2 in binary is 10
++  11  // 3 in binary is 11
++ 100  // 4 in binary is 100
+-----
+  122  // 122 in binary is 1111010
+*/
+
+function binaryPyramid(m, n) {
+    let result = 0;
+    for (let i = m; i <= n; i++) {
+        result += Number(i.toString(2))
+    }
+    return result.toString(2);
+}
