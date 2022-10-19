bd826495011a2ad57b4feb760ce73203d470897a
Lea Brown <LeaBrown198x@gmail.com>
2022-10-19 17:42:10 +0100

    codewars 19/10

---
+++ 7kyu/Consecutive-Vowel.js
@@ -0,0 +1,48 @@
+/* 7 kyu
+Consecutive Vowels in a String
+
+https://www.codewars.com/kata/62a933d6d6deb7001093de16/javascript
+
+DESCRIPTION:
+Consecutive Vowels
+You are given a random string of lower-case letters. Your job is to find out how many ordered and consecutive vowels there are in the given string beginning from 'a'. Keep in mind that the consecutive vowel to 'u' is 'a' and the cycle continues.
+
+Return an integer with the length of the consecutive vowels found.
+
+This is better explained with a couple of examples:
+
+You are given the string "agrtertyfikfmroyrntbvsukldkfa". The logic is that you start from the 'a' and make your way right. The next vowel is an 'e' and it is the consecutive vowel, then 'i' and so forth until you get to 'u'. If you keep moving right you find 'a' which happens to be the consecutive vowel. Return 6.
+This is a slightly trickier example: you are given the string "erfaiekjudhyfimngukduo". As you move right you ignore all vowels until you get to the 'a', then ignore the rest until you get to the 'e', which is the consecutive vowel, and so forth until you get to the 'o'. Return 4.
+Note
+For this kata, the vowels are 'a', 'e', 'i', 'o', 'u', in that order. 'y' is not considered a vowel in this kata.
+*/
+
+function getTheVowels(word) {
+    let vowsArr = word.match(/[aeiou]/g);
+    let count = 0;
+    let lastLetter;
+    vowsArr.forEach(e => {
+        if (e == 'a' && count == 0) {
+            count++;
+            lastLetter = 'a';
+        }
+
+        if (e == 'a' && lastLetter == 'u') {
+            count++;
+            lastLetter = 'a';
+        } else if (e == 'e' && lastLetter == 'a') {
+            count++;
+            lastLetter = 'e';
+        } else if (e == 'i' && lastLetter == 'e') {
+            count++;
+            lastLetter = 'i';
+        } else if (e == 'o' && lastLetter == 'i') {
+            count++;
+            lastLetter = 'o';
+        } else if (e == 'u' && lastLetter == 'o') {
+            count++;
+            lastLetter = 'u';
+        }
+    })
+    return count;
+}
