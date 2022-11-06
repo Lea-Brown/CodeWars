ca48337e71a8177f7557b074129f3150fadc5da3
Lea Brown <LeaBrown198x@gmail.com>
2022-11-06 17:33:39 +0000

    codewars 6/11

---
+++ 7kyu/Round-up-to-the-next-multiple-of-5.js
@@ -0,0 +1,31 @@
+/* 7 kyu
+Round up to the next multiple of 5
+
+https://www.codewars.com/kata/55d1d6d5955ec6365400006d/javascript
+
+DESCRIPTION:
+Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+Examples:
+
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+Input may be any positive or negative integer (including 0).
+
+You can assume that all inputs are valid integers.
+*/
+
+function roundToNext5(n) {
+    while (n % 5 != 0) {
+        n++
+    }
+    return n
+}
