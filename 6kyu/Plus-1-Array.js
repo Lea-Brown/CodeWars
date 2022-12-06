e8206a61e93ad9fcfd3458eea7015e8343571c00
Lea Brown <LeaBrown198x@gmail.com>
2022-12-06 14:12:25 +0000

    codewars 6/12

---
+++ 6kyu/Plus-1-Array.js
@@ -0,0 +1,41 @@
+/* 6 kyu
++1 Array
+
+https://www.codewars.com/kata/5514e5b77e6b2f38e0000ca9/javascript
+
+Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
+
+the array can't be empty
+only non-negative, single digit integers are allowed
+Return nil (or your language's equivalent) for invalid inputs.
+
+Examples
+Valid arrays
+
+[4, 3, 2, 5] would return [4, 3, 2, 6]
+[1, 2, 3, 9] would return [1, 2, 4, 0]
+[9, 9, 9, 9] would return [1, 0, 0, 0, 0]
+[0, 1, 3, 7] would return [0, 1, 3, 8]
+
+Invalid arrays
+
+[1, -9] is invalid because -9 is not a non-negative integer
+
+[1, 2, 33] is invalid because 33 is not a single-digit integer
+*/
+
+function upArray(arr) {
+    let result = arr.reverse();
+    result.forEach((e, i) => {
+        if (i === 0) result[i] += 1;
+        if (result[i] > 9) {
+            result[i] = 0;
+            if (typeof result[i + 1] === 'number') {
+                result[i + 1] += 1;
+            } else {
+                result.push(1);
+            }
+        }
+    })
+    return result.reverse();
+}
