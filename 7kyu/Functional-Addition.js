b822af7a8f9f9485734abda9857d0a0ebbd79ae2
Lea Brown <LeaBrown198x@gmail.com>
2022-12-15 19:15:24 +0000

    codewars 15/12

---
+++ 7kyu/Functional-Addition.js
@@ -0,0 +1,17 @@
+/* 7 kyu
+Functional Addition
+
+https://www.codewars.com/kata/538835ae443aae6e03000547/javascript
+
+Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+var addOne = add(1);
+addOne(3); // 4
+
+var addThree = add(3);
+addThree(3); // 6
+*/
+
+let add = (n) => (nN) => n + nN
