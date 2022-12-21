523c598535202ca58e758bcd3e28152e84b92b75
Lea Brown <LeaBrown198x@gmail.com>
2022-12-21 21:08:42 +0000

    codewars 21/12

---
+++ 8kyu/Hello-Name-or-World.js
@@ -0,0 +1,20 @@
+/* 8 kyu
+Hello, Name or World!
+
+https://www.codewars.com/kata/57e3f79c9cb119374600046b/javascript
+
+Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+Examples:
+
+* With `name` = "john"  => return "Hello, John!"
+* With `name` = "aliCE" => return "Hello, Alice!"
+* With `name` not given 
+  or `name` = ""        => return "Hello, World!"
+*/
+
+function hello(name) {
+  return !name ? 'Hello, World!' : `Hello, ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`
+}
