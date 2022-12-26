62c543f43e4fe4597ba1e68581a83020f123dd3f
Lea Brown <LeaBrown198x@gmail.com>
2022-12-26 13:10:34 +0000

    codewars 26/12

---
+++ 7kyu/Sums-of-a-lot-of-numbers.js
@@ -0,0 +1,27 @@
+/* 7 kyu
+Gauß needs help! (Sums of a lot of numbers).
+
+https://www.codewars.com/kata/54df2067ecaa226eca000229/javascript
+
+Due to another of his misbehaved, the primary school's teacher of the young Gauß, Herr J.G. Büttner, to keep the bored and unruly young schoolboy Karl Friedrich Gauss busy for a good long time, while he teaching arithmetic to his mates, assigned him the problem of adding up all the whole numbers from 1 through a given number n.
+
+Your task is to help the young Carl Friedrich to solve this problem as quickly as you can; so, he can astonish his teacher and rescue his recreation interval.
+
+Here's, an example:
+
+f(n=100) // returns 5050 
+It's your duty to verify that n is a valid positive integer number. If not, please, return false (None for Python, null for C#, 0 for COBOL).
+
+Note: the goal of this kata is to invite you to think about some 'basic' mathematic formula and how you can do performance optimization on your code. 
+
+Advanced - experienced users should try to solve it in one line, without loops, or optimizing the code as much as they can.
+*/
+
+function f(n) {
+    if (typeof n != 'number' || n < 1 || n % 1 != 0) return false
+    let count = 0
+    for (let i = 1; i <= n; i++) {
+        count += i
+    }
+    return count
+};
