8f5a5c47f41e32746023c83d53b47bd265ef90df
Lea Brown <LeaBrown198x@gmail.com>
2023-01-20 12:06:47 +0000

    codewars 20/1

---
+++ 7kyu/Flatten.js
@@ -0,0 +1,13 @@
+/* 7 kyu
+Flatten
+
+https://www.codewars.com/kata/5250a89b1625e5decd000413/javascript
+
+Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+flatten([1,2,3]) // => [1,2,3]
+flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+flatten([[[1,2,3]]]) // => [[1,2,3]]
+*/
+
+const flatten = array => array.flat();
