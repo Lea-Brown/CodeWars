24e976139800826ae5d874ab05330d6177c28315
Lea Brown <LeaBrown198x@gmail.com>
2023-02-02 15:26:42 +0000

    codewars 2/2

---
+++ 7kyu/All-unique.js
@@ -0,0 +1,13 @@
+/* 7 kyu
+All unique
+
+https://www.codewars.com/kata/553e8b195b853c6db4000048/javascript
+
+Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+
+The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+*/
+
+function hasUniqueChars(str) {
+    return [...new Set(str)].length === str.length;
+}
