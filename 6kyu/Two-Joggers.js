728a388db8ebe6dc379060ca6af1c3a43e674bf0
Lea Brown <LeaBrown198x@gmail.com>
2023-02-26 17:12:13 +0000

    codewars 26/2

---
+++ 6kyu/Two-Joggers.js
@@ -0,0 +1,43 @@
+/* 6 kyu
+Two Joggers
+
+https://www.codewars.com/kata/5274d9d3ebc3030802000165/javascript
+
+Bob and Charles are meeting for their weekly jogging tour. They both start at the same spot called "Start" and they each run a different lap, which may (or may not) vary in length. Since they know each other for a long time already, they both run at the exact same speed.
+
+Task
+Your job is to complete the function nbrOfLaps(x, y) that, given the length of the laps for Bob and Charles, finds the number of laps that each jogger has to complete before they meet each other again, at the same time, at the start.
+
+The function takes two arguments:
+
+The length of Bob's lap (larger than 0)
+The length of Charles' lap (larger than 0)
+
+The function should return an array (Tuple<int, int> in C#) containing exactly two numbers:
+
+The first number is the number of laps that Bob has to run
+The second number is the number of laps that Charles has to run
+
+Examples:
+
+nbrOfLaps(5, 3); // returns [3, 5]
+nbrOfLaps(4, 6); // returns [3, 2]
+*/
+
+var nbrOfLaps = function (x, y) {
+    let one = 1;
+    let two = 1;
+    let newX = x;
+    let newY = y;
+    while (newX != newY) {
+        if (newX < newY) {
+            newX += x;
+            one++;
+        }
+        if (newY < newX) {
+            newY += y;
+            two++;
+        }
+    }
+    return [one, two];
+}
