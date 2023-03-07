b83252d735a69cfb9e08fcc03233ea8cd17931bb
Lea Brown <LeaBrown198x@gmail.com>
2023-03-07 20:10:44 +0000

    codewars 7/3

---
+++ 8kyu/The-if-function.js
@@ -0,0 +1,17 @@
+/* 8 kyu
+The 'if' function
+
+https://www.codewars.com/kata/54147087d5c2ebe4f1000805/javascript
+
+Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
+
+When bool is truthy, func1 should be called, otherwise call the func2.
+
+Example:
+_if(true, function(){console.log("True")}, function(){console.log("false")})
+// Logs 'True' to the console.
+*/
+
+function _if(bool, func1, func2) {
+    return bool == true ? func1() : func2()
+}
