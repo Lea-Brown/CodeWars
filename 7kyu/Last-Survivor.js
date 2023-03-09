58fa7b809f63e055e605f863eb7938dbc34d6607
Lea Brown <LeaBrown198x@gmail.com>
2023-03-09 20:44:12 +0000

    codewars 9/3

---
+++ 7kyu/Last-Survivor.js
@@ -0,0 +1,28 @@
+/* 7 kyu
+Last Survivor
+
+https://www.codewars.com/kata/609eee71109f860006c377d1/javascript
+
+You are given a string of letters and an array of numbers.
+The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
+After each removal the size of the string decreases (there is no empty space).
+Return the only letter left.
+
+Example:
+
+let str = "zbk", arr = [0, 1]
+    str = "bk", arr = [1]
+    str = "b", arr = []
+    return 'b'
+Notes
+The given string will never be empty.
+The length of the array is always one less than the length of the string.
+All numbers are valid.
+There can be duplicate letters and numbers.
+*/
+
+function lastSurvivor(letters, coords) {
+    let letter = [...letters];
+    coords.forEach(e => letter.splice(e, 1));
+    return letter[0];
+}
