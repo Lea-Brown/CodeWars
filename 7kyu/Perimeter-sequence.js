a66b76401fffca5e51aa6c72649de4259c9f3c35
Lea Brown <LeaBrown198x@gmail.com>
2023-04-03 11:16:07 +0100

    codewars 3/4

---
+++ 7kyu/Perimeter-sequence.js
@@ -0,0 +1,11 @@
+/* 7 kyu
+Perimeter sequence
+
+https://www.codewars.com/kata/589519d1f0902e01af000054/javascript
+
+The blocksize is a by a and a ≥ 1.
+
+What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+*/
+
+const perimeterSequence = (a, n) => n * 4 * a;
