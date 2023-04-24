49d238849e2729d6bc504e2bddc25cccc9da0d18
unknown <leabrown198x@gmail.com>
2023-04-24 14:18:52 +0100

    codewars 24/4

---
+++ 7kyu/Indexed-capitalization.js
@@ -0,0 +1,16 @@
+/* 7 kyu
+Indexed capitalization
+
+https://www.codewars.com/kata/59cfc09a86a6fdf6df0000f1/javascript
+
+Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+For example:
+
+capitalize("abcdef",[1,2,5]) = "aBCdeF"
+capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+The input will be a lowercase string with no spaces and an array of digits.
+*/
+
+const capitalize = (s, arr) =>
+  [...s].map((e, i) => (arr.includes(i) ? e.toUpperCase() : e)).join("");
