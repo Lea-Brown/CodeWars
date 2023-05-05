d123518a0c0b8149edb96ee8e62207286909b499
unknown <leabrown198x@gmail.com>
2023-05-05 16:27:10 +0100

    codewars 5/5

---
+++ 8kyu/Generate-range-of-integers.js
@@ -0,0 +1,25 @@
+/* 8 kyu
+Generate range of integers
+
+https://www.codewars.com/kata/55eca815d0d20962e1000106/javascript
+
+Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+
+Task
+Implement a function named
+
+generateRange(2, 10, 2) // should return array of [2,4,6,8,10]
+generateRange(1, 10, 3) // should return array of [1,4,7,10]
+Note
+min < max
+step > 0
+the range does not HAVE to include max (depending on the step)
+*/
+
+function generateRange(min, max, step) {
+	let result = [];
+	for (let i = min; i <= max; i += step) {
+		result.push(i);
+	}
+	return result;
+}
