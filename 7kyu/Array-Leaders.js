a5d344e22cacc8ed3120c63c1c14c49e9a91f088
unknown <leabrown198x@gmail.com>
2023-05-09 16:52:29 +0100

    codewars 9/5

---
+++ 7kyu/Array-Leaders.js
@@ -0,0 +1,78 @@
+/* 7 kyu
+Array Leaders (Array Series #3)
+
+https://www.codewars.com/kata/5a651865fd56cb55760000e0/javascript
+
+An element is leader if it is greater than The Sum all the elements to its right side.
+
+Task
+Given an array/list [] of integers , Find all the LEADERS in the array.
+
+Notes
+Array/list size is at least 3 .
+
+Array/list's numbers Will be mixture of positives , negatives and zeros
+
+Repetition of numbers in the array/list could occur.
+
+Returned Array/list should store the leading numbers in the same order in the original array/list .
+
+Input >> Output Examples
+arrayLeaders ({1, 2, 3, 4, 0}) ==> return {4}
+Explanation:
+4 is greater than the sum all the elements to its right side
+
+Note : The last element 0 is equal to right sum of its elements (abstract zero).
+
+arrayLeaders ({16, 17, 4, 3, 5, 2}) ==> return {17, 5, 2}
+Explanation:
+17 is greater than the sum all the elements to its right side
+
+5 is greater than the sum all the elements to its right side
+
+Note : The last element 2 is greater than the sum of its right elements (abstract zero).
+
+arrayLeaders ({5, 2, -1}) ==> return {5, 2}
+Explanation:
+5 is greater than the sum all the elements to its right side
+
+2 is greater than the sum all the elements to its right side
+
+Note : The last element -1 is less than the sum of its right elements (abstract zero).
+
+arrayLeaders ({0, -1, -29, 3, 2}) ==> return {0, -1, 3, 2}
+Explanation:
+0 is greater than the sum all the elements to its right side
+
+-1 is greater than the sum all the elements to its right side
+
+3 is greater than the sum all the elements to its right side
+
+Note : The last element 2 is greater than the sum of its right elements (abstract zero).
+*/
+
+function arrayLeaders(numbers) {
+	let result = [];
+	numbers.forEach((e, i) => {
+		if (e > numbers.slice(-(numbers.length - i)).reduce((a, b) => a + b) - e) result.push(e);
+	});
+	return result;
+}
+
+// refractored by chat gpt
+
+function findLeaders(numbers) {
+	const leaders = [];
+	let rightSum = 0;
+
+	// Iterate over the array from right to left
+	for (let i = numbers.length - 1; i >= 0; i--) {
+		// Check if the current element is greater than the sum of elements to its right
+		if (numbers[i] > rightSum) {
+			leaders.unshift(numbers[i]); // Add the current element to the beginning of leaders array
+		}
+		rightSum += numbers[i]; // Update the sum of elements to the right
+	}
+
+	return leaders;
+}
