eaa0089ff0973d7e43800deb08cd676100f13835
Lea-Brown <LeaBrown198x@gmail.com>
2023-06-01 21:18:55 +0100

    codewars 1/6

---
+++ 7kyu/Is-Ruby-coming.js
@@ -0,0 +1,31 @@
+/* 7 kyu
+Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming?
+
+https://www.codewars.com/kata/5827acd5f524dd029d0005a4/javascript
+
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if at least one Ruby developer has signed up; or
+false if there will be no Ruby developers.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Ruby' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+];
+your function should return true.
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+*/
+
+function isRubyComing(list) {
+  for (let i = 0; i < list.length; i++) {
+    if (Object.values(list[i]).includes('Ruby')) return true;
+  }
+  return false;
+}
