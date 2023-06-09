2318a65b5db442b3c2147030abb1b1c5d12b0fbe
Lea-Brown <LeaBrown198x@gmail.com>
2023-06-09 11:40:19 +0100

    codewars 9/6

---
+++ 8kyu/Take-the-First-N-Elements.js
@@ -0,0 +1,9 @@
+/*  8 kyu
+Enumerable Magic #25 - Take the First N Elements
+
+https://www.codewars.com/kata/545afd0761aa4c3055001386/javascript
+
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+*/
+
+const take = (arr, n) => arr.slice(0, n);
