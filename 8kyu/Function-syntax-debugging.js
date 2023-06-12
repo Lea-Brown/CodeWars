1b257344d8bc4deba19088bf17c6210e9156622a
Lea-Brown <LeaBrown198x@gmail.com>
2023-06-12 17:24:56 +0100

    codewars 12/6

---
+++ 8kyu/Function-syntax-debugging.js
@@ -0,0 +1,9 @@
+/* 8 kyu
+Grasshopper - Function syntax debugging
+
+https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/javascript
+
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+*/
+
+const main = (verb, noun) => verb + noun;
