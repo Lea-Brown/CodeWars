0bdfa6cd4791539a570d490816147505b794f1f9
Lea-Brown <LeaBrown198x@gmail.com>
2023-06-23 13:58:49 +0100

    codewars 23/6

---
+++ 8kyu/Find-the-position.js
@@ -0,0 +1,15 @@
+/* 8 kyu
+Find the position!
+
+https://www.codewars.com/kata/5808e2006b65bff35500008f/javascript
+
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+*/
+
+function position(letter) {
+  return `Position of alphabet: ${letter.charCodeAt(0) - 96}`;
+}
