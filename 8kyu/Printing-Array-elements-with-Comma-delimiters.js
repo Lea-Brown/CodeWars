163820b48f3b61647e3ef6ebfe9bcfea8cd88f5a
Lea <leabrown198x@gmail.com>
2023-06-27 19:31:59 +0100

    codewars 27/6

--- 8kyu/Printing-Array-elements-with-Comma-delimiters.js
+++ 8kyu/Printing-Array-elements-with-Comma-delimiters.js
@@ -5,7 +5,7 @@ https://www.codewars.com/kata/56e2f59fb2ed128081001328/javascript
 
 Input: Array of elements
 
-["h","o","l","a"]
+["h","o","l","a"] 
 
 Output: String with comma delimited elements of the array in th same order.
 
