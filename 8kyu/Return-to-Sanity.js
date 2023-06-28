de1f8fde78b3c2c3d871e58a7c5e7e1b9dab06b8
Lea <leabrown198x@gmail.com>
2023-06-28 06:34:29 +0100

    codewars 28/6

---
+++ 8kyu/Return-to-Sanity.js
@@ -0,0 +1,10 @@
+/* 8 kyu
+Return to Sanity
+
+https://www.codewars.com/kata/514a7ac1a33775cbb500001e/javascript
+*/
+
+function mystery() {
+  const results = { sanity: 'Hello' };
+  return results;
+}
