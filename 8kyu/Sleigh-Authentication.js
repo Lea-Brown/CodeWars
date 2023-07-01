452116a851ac218e49cc3884cd578620417f5945
Lea <leabrown198x@gmail.com>
2023-07-01 16:58:00 +0100

    codewars 1/7

---
+++ 8kyu/Sleigh-Authentication.js
@@ -0,0 +1,11 @@
+/* 8 kyu
+Sleigh Authentication
+
+https://www.codewars.com/kata/52adc142b2651f25a8000643/javascript
+*/
+
+function Sleigh() {}
+
+Sleigh.prototype.authenticate = function (name, password) {
+  return name == 'Santa Claus' && password == 'Ho Ho Ho!';
+};
