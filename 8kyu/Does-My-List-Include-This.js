8bbba2b654bf478a294b47ec3b5422155ad1d8b1
Lea <leabrown198x@gmail.com>
2023-07-03 15:27:21 +0100

    codewars 3/7

---
+++ 8kyu/Does-My-List-Include-This.js
@@ -0,0 +1,11 @@
+/* 8 kyu
+Enumerable Magic - Does My List Include This?
+
+https://www.codewars.com/kata/545991b4cbae2a5fda000158/javascript
+
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+
+function include(arr, item) {
+  return arr.includes(item);
+}
