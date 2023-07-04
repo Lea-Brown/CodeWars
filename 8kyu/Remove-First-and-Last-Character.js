ad950114fddb97bcdaea4b262c63160efa5bc9c2
Lea <leabrown198x@gmail.com>
2023-07-04 21:18:40 +0100

    codewars 4/7

---
+++ 8kyu/Remove-First-and-Last-Character.js
@@ -0,0 +1,25 @@
+/* 8 kyu
+Remove First and Last Character Part Two
+
+https://www.codewars.com/kata/570597e258b58f6edc00230d/javascript
+
+You are given a string containing a sequence of character sequences separated by commas.
+
+Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+Examples
+"1,2,3"      =>  "2"
+"1,2,3,4"    =>  "2 3"
+"1,2,3,4,5"  =>  "2 3 4"
+
+""     =>  NULL
+"1"    =>  NULL
+"1,2"  =>  NULL
+*/
+
+function array(string) {
+  const arr = string.split(',');
+  return arr.length > 2 ? arr.slice(1, arr.length - 1).join(' ') : null;
+}
