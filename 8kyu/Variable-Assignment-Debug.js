700d7dc0ead6c659f6d9315c842e187b2cf1a365
Lea <leabrown198x@gmail.com>
2023-07-08 18:20:09 +0100

    codewars 8/7

---
+++ 8kyu/Variable-Assignment-Debug.js
@@ -0,0 +1,11 @@
+/* 8 kyu
+Grasshopper - Variable Assignment Debug
+
+https://www.codewars.com/kata/5612e743cab69fec6d000077/javascript
+
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+*/
+
+let a = 'dev';
+let b = 'Lab';
+let name = `${a}${b}`;
