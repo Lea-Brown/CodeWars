7e850747e528fa8c65ea723af59f03e0ff211e32
Lea <leabrown198x@gmail.com>
2023-07-15 13:32:19 +0100

    codewars 15/7

---
+++ 8kyu/Add-item-to-an-Array.js
@@ -0,0 +1,14 @@
+/* 8 kyu
+Basic Training: Add item to an Array
+
+https://www.codewars.com/kata/511f0fe64ae8683297000001/javascript
+
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+*/
+
+websites[0] = 'codewars';
