5f5aae66e7fe9c316d57a21bdcf5933213045a87
Lea <leabrown198x@gmail.com>
2023-07-25 11:51:26 +0100

    codewars 25/7

---
+++ 8kyu/Type-of-sum.js
@@ -0,0 +1,9 @@
+/* 8 kyu
+Type of sum
+
+https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba/javascript
+
+Return the type of the sum of the two arguments
+*/
+
+const typeOfSum = (a, b) => typeof (a + b);
