0e31e5a30153a30ac95f1a909887752e7d92ba5e
Lea <leabrown198x@gmail.com>
2023-08-15 11:37:31 +0100

    codewars 15/8

---
+++ 8kyu/Return.js
@@ -0,0 +1,35 @@
+/* 8 kyu
+Fundamentals: Return
+
+https://www.codewars.com/kata/55a5befdf16499bffb00007b/javascript
+
+Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+Please use the following function names:
+
+addition = add
+
+multiply = multiply
+
+division = divide (both integer and float divisions are accepted)
+
+modulus = mod
+
+exponential = exponent
+
+subtraction = subt
+
+Note: All math operations will be: a (operation) b
+*/
+
+const add = (a, b) => a + b;
+
+const divide = (a, b) => a / b;
+
+const multiply = (a, b) => a * b;
+
+const mod = (a, b) => a % b;
+
+const exponent = (a, b) => a ** b;
+
+const subt = (a, b) => a - b;
