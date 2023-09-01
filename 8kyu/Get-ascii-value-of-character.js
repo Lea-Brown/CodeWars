3fe9f0fe92f9a943e2fd149bfa9533e3cb00cde8
Lea <leabrown198x@gmail.com>
2023-09-01 18:32:15 +0100

    codewars 1/9

---
+++ 8kyu/Get-ascii-value-of-character.js
@@ -0,0 +1,13 @@
+/* 8 kyu
+Get ascii value of character
+
+https://www.codewars.com/kata/55acfc59c3c23d230f00006d/javascript
+
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/
+*/
+
+function getASCII(c) {
+  return c.charCodeAt(0);
+}
