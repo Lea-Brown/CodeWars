60acf46f1cd0993fc33aec9b3f2625c7bf8ab8e0
Lea <leabrown198x@gmail.com>
2023-11-02 20:52:44 +0000

    codewars 2/11

---
+++ 8kyu/Shifty-Closures.js
@@ -0,0 +1,12 @@
+/* 8 kyu
+Shifty Closures
+
+https://www.codewars.com/kata/514aa0dc21607ae236000017/javascript
+
+Functional closures can get overly attached. Set them straight!
+
+Why doesn't greet_abe() actually greet Abe?
+*/
+
+const greet_abe = () => 'Hello, Abe!';
+const greet_ben = () => 'Hello, Ben!';
