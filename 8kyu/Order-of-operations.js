2dba97fdc8569485ad30e275789be296a4021906
Lea <leabrown198x@gmail.com>
2023-11-08 19:21:50 +0000

    codewars 8/11

---
+++ 8kyu/Order-of-operations.js
@@ -0,0 +1,9 @@
+/* 8 kyu
+Grasshopper - Order of operations
+
+https://www.codewars.com/kata/560ecf0cb040de130e00007d
+
+You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 32 but instead the function keeps returning 10. Fix the function to make it return 32 without changing the number or the operators.
+*/
+
+const orderOperations = () => (2 + 2) * (2 + 2) * 2;
