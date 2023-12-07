0adfd1f6f8343b62a761d347eeb9cb235b40a98b
Lea <leabrown198x@gmail.com>
2023-12-07 16:24:10 +0000

    codewars 7/12

---
+++ 8kyu/Polish-alphabet.js
@@ -0,0 +1,39 @@
+/* 8 kyu
+Polish alphabet
+
+https://www.codewars.com/kata/57ab2d6072292dbf7c000039/javascript
+
+There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+Your task is to change the letters with diacritics:
+
+ą -> a,
+ć -> c,
+ę -> e,
+ł -> l,
+ń -> n,
+ó -> o,
+ś -> s,
+ź -> z,
+ż -> z
+and print out the string without the use of the Polish letters.
+
+For example:
+
+"Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+*/
+
+function correctPolishLetters(string) {
+  const change = {
+    ą: 'a',
+    ć: 'c',
+    ę: 'e',
+    ł: 'l',
+    ń: 'n',
+    ó: 'o',
+    ś: 's',
+    ź: 'z',
+    ż: 'z',
+  };
+  return [...string].map(e => (change[e] ? change[e] : e)).join('');
+}
