b111825e8563ae888cc51da25cb58875fa106e39
Lea <leabrown198x@gmail.com>
2024-02-04 19:50:30 +0000

    codewars 4/2

---
+++ 8kyu/Find-variable-which-breaks-strict-comparison.js
@@ -0,0 +1,13 @@
+/* 8 kyu
+Find variable which breaks strict comparison!
+
+https://www.codewars.com/kata/560f8d41cf6e1fe5c900002e/javascript
+
+In JavaScript, there is a special case where strict comparison of the same variable returns false! Try to find out what must be done to get such result!
+
+var x = something;
+x === x // returns false!
+Write a function which will return value for which strict comparison will give false!
+*/
+
+const findStrangeValue = () => NaN;
