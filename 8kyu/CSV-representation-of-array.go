e42d0b6b01fd792c62f31e05837c3411843c2747
Lea-Brown <leabrown198x@gmail.com>
2024-02-22 12:36:45 +0000

    codewars 22/2

---
+++ 8kyu/CSV-representation-of-array.go
@@ -0,0 +1,55 @@
+/* 8 kyu
+CSV representation of array
+
+https://www.codewars.com/kata/5a34af40e1ce0eb1f5000036/go
+
+Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+Example:
+
+input:
+   [[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]]
+
+output:
+     '0,1,2,3,4\n'
+    +'10,11,12,13,14\n'
+    +'20,21,22,23,24\n'
+    +'30,31,32,33,34'
+Array's length > 2.
+
+More details here: https://en.wikipedia.org/wiki/Comma-separated_values
+
+Note: you shouldn't escape the \n, it should work as a new line.
+*/
+
+// Helped by chatGPT
+
+package kata
+
+import (
+	"strconv"
+	"strings"
+)
+
+func ToCsvText(array [][]int) string {
+	var result strings.Builder
+
+	for i, row := range array {
+		for j, value := range row {
+			result.WriteString(strconv.Itoa(value))
+
+			if j < len(row)-1 {
+				result.WriteRune(',')
+			}
+		}
+
+		if i < len(array)-1 {
+			result.WriteRune('\n')
+		}
+	}
+
+	return result.String()
+}
