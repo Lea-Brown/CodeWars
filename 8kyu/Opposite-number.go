baf28ad273549620add68803ae881030cfc4588e
Lea-Brown <leabrown198x@gmail.com>
2024-02-25 12:28:14 +0000

    codewars 25/2

---
+++ 8kyu/Opposite-number.go
@@ -0,0 +1,19 @@
+/* 8 kyu
+Opposite number
+
+https://www.codewars.com/kata/56dec885c54a926dcd001095/go
+
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+package kata
+
+func Opposite(value int) int {
+	return -value
+}
