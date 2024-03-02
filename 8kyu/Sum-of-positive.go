d048cc4466740a3b01dbe2fc2b244a867669d7d6
Lea-Brown <leabrown198x@gmail.com>
2024-03-02 18:45:01 +0000

    codewars 2/3

---
+++ 8kyu/Sum-of-positive.go
@@ -0,0 +1,25 @@
+/* 8 kyu
+Sum of positive
+
+https://www.codewars.com/kata/5715eaedb436cf5606000381/go
+
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+package kata
+
+func PositiveSum(numbers []int) int {
+	result := 0
+
+	for i := 0; i < len(numbers); i++ {
+		if numbers[i] > 0 {
+			result += numbers[i]
+		}
+	}
+
+	return result
+}
