b68f0578231c14bad661941bc816a356d9016371
Lea-Brown <leabrown198x@gmail.com>
2024-03-06 15:05:12 +0000

    codewars 6/3

---
+++ 8kyu/Find-smallest-integer-in-array.go
@@ -0,0 +1,25 @@
+/* 8 kyu
+Find the smallest integer in the array
+
+https://www.codewars.com/kata/55a2d7ebe362935a210000b2/go
+
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+*/
+
+package kata
+
+func SmallestIntegerFinder(numbers []int) int {
+	result := numbers[0]
+	for _, val := range numbers {
+		if val < result {
+			result = val
+		}
+	}
+	return result
+}
