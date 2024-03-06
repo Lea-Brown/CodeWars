bbb408f6cf56a2efc2d3ba6073ec6ee7875df84f
Lea-Brown <leabrown198x@gmail.com>
2024-03-06 15:11:52 +0000

    codewars 6/3

--- 8kyu/Find-smallest-integer-in-array.go
+++ 8kyu/Find-smallest-integer-in-array.go
@@ -23,3 +23,11 @@ func SmallestIntegerFinder(numbers []int) int {
 	}
 	return result
 }
+
+package kata
+import "sort"
+
+func SmallestIntegerFinder(numbers []int) int {
+	sort.Ints(numbers)
+	return numbers[0]
+}
