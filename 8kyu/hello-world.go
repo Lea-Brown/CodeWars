98b5a97672c2e120af9a057b9a4213907b6de966
Lea-Brown <leabrown198x@gmail.com>
2024-03-09 14:22:30 +0000

    codewars 9/3

---
+++ 8kyu/hello-world.go
@@ -0,0 +1,13 @@
+/* 8 kyu
+Function 1 - hello world
+
+https://www.codewars.com/kata/523b4ff7adca849afe000035/go
+
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+*/
+
+package kata
+
+func greet() string {
+	return "hello world!"
+}
