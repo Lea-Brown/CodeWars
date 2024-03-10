7005d93bd25e8d0f319c5937c6f2c93fa8d2df9e
Lea-Brown <leabrown198x@gmail.com>
2024-03-10 14:27:37 +0000

    codewars 10/3

---
+++ 8kyu/Counting-sheep.go
@@ -0,0 +1,30 @@
+/* 8 kyu
+Counting sheep...
+
+https://www.codewars.com/kata/54edbc7200b811e956000556/go
+
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+*/
+
+package kata
+
+func CountSheeps(numbers []bool) (result int) {
+	for _, e := range numbers {
+		if e == true {
+			result++
+		}
+	}
+	return
+}
