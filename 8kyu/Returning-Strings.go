69547cc4bee43d167bfe3f38cdc468671ac66d2e
Lea-Brown <leabrown198x@gmail.com>
2024-03-13 17:14:59 +0000

    codewars 13/3

---
+++ 8kyu/Returning-Strings.go
@@ -0,0 +1,15 @@
+/* 8 kyu
+Returning Strings
+
+https://www.codewars.com/kata/55a70521798b14d4750000a4/go
+
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+
+package kata
+
+func Greet(name string) string {
+	return "Hello, " + name + " how are you doing today?"
+}
