0c8eb3fef362b85c3726a148dad90d42ed37820d
Lea-Brown <leabrown198x@gmail.com>
2024-03-20 13:48:50 +0000

    codewars 20/3

---
+++ 8kyu/MakeUpperCase.go
@@ -0,0 +1,15 @@
+/* 8 kyu
+MakeUpperCase
+
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/go
+
+Write a function which converts the input string to uppercase.
+*/
+
+package kata
+
+import "strings"
+
+func MakeUpperCase(str string) string {
+	return strings.ToUpper(str)
+}
