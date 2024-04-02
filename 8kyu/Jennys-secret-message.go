1236f0b6a2b02e4ffc5fd7c2a6d53f03edd8e579
Lea-Brown <leabrown198x@gmail.com>
2024-04-02 16:09:20 +0100

    codewars 2/4

---
+++ 8kyu/Jennys-secret-message.go
@@ -0,0 +1,20 @@
+/* 8 kyu
+Jenny's secret message
+
+https://www.codewars.com/kata/55225023e1be1ec8bc000390/go
+
+Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+Can you help her?
+*/
+
+package kata
+
+import "fmt"
+
+func Greet(name string) string {
+	if name == "Johnny" {
+		return fmt.Sprint("Hello, my love!")
+	}
+	return fmt.Sprintf("Hello, %v!", name)
+}
