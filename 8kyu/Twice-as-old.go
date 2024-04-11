09c37cb76fedb1b1e00b08f51c068ac340d100e6
Lea-Brown <leabrown198x@gmail.com>
2024-04-11 12:50:54 +0100

    codewars 11/4

---
+++ 8kyu/Twice-as-old.go
@@ -0,0 +1,19 @@
+/* 8 kyu
+Twice as old
+
+https://www.codewars.com/kata/5b853229cfde412a470000d0/go
+
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+*/
+
+package kata
+
+import "math"
+
+func TwiceAsOld(dadYearsOld, sonYearsOld int) int {
+	return int(math.Abs(float64(dadYearsOld - (sonYearsOld * 2))))
+}
