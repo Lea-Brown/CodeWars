a434d27e2db9a472964cbf5ba7138e93993067ea
Lea-Brown <leabrown198x@gmail.com>
2024-04-16 16:30:56 +0100

    codewars 16/4

---
+++ 8kyu/Is-the-string-uppercase.go
@@ -0,0 +1,31 @@
+/* 8 kyu
+Is the string uppercase?
+
+https://www.codewars.com/kata/56cd44e1aa4ac7879200010b
+
+Create a method to see whether the string is ALL CAPS.
+
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+*/
+
+package kata
+
+import "unicode"
+
+type MyString string
+
+func (s MyString) IsUpperCase() bool {
+	for _, char := range s {
+		if !unicode.IsUpper(char) && unicode.IsLetter(char) {
+			return false
+		}
+	}
+	return true
+}
