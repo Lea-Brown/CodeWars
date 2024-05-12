bf5df24f3b4fe9179e479d79b22114a5407017f1
Lea-Brown <leabrown198x@gmail.com>
2024-05-12 20:53:17 +0100

    codewars 12/5

---
+++ 7kyu/Disemvowel-Trolls.go
@@ -0,0 +1,31 @@
+/* 7 kyu
+Disemvowel Trolls
+
+https://www.codewars.com/kata/52fba66badcd10859f00097e/go
+
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+*/
+
+package kata
+
+import "strings"
+
+func Disemvowel(str string) string {
+	vowels := "aeiouAEIOU"
+	result := strings.Builder{}
+
+	for _, char := range str {
+		if !strings.ContainsRune(vowels, char) {
+			result.WriteRune(char)
+		}
+	}
+	return result.String()
+}
