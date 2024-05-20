5d21e51f018b6e99549f5d757781cadda8ba9e3e
Lea-Brown <leabrown198x@gmail.com>
2024-05-20 12:39:11 +0100

    codewars 20/5

---
+++ 7kyu/Sum-of-Numbers.go
@@ -0,0 +1,34 @@
+/* 7 kyu
+Beginner Series #3 Sum of Numbers
+
+https://www.codewars.com/kata/55f2b110f61eb01779000053/go
+
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+Your function should only return a number, not the explanation about how you get that number.
+*/
+
+package kata
+
+func GetSum(a, b int) int {
+	var minVal, maxVal int
+
+	if a < b {
+		minVal = a
+		maxVal = b
+	} else {
+		minVal = b
+		maxVal = a
+	}
+
+	return (maxVal - minVal + 1) * (minVal + maxVal) / 2
+}
