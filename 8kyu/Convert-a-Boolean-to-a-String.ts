221d64d55cb4a0f2bcb5a31af1b334d08d593e96
Lea <leabrown198x@gmail.com>
2025-03-01 19:12:06 +0000

    codewars 1/3

---
+++ 8kyu/Convert-a-Boolean-to-a-String.ts
@@ -0,0 +1,13 @@
+/* 8 kyu
+Convert a Boolean to a String
+
+https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/typescript
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+export const booleanToString = (b: boolean): string => {
+  return b.toString();
+};
