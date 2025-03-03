e73b310280f2fd8a278db7327298e7ae7e65c3d3
Lea <leabrown198x@gmail.com>
2025-03-03 16:46:02 +0000

    codewars 3/3

---
+++ 8kyu/Multiply.ts
@@ -0,0 +1,11 @@
+/* 8 kyu
+Multiply
+
+https://www.codewars.com/kata/50654ddff44f800200000004/train/typescript
+
+This code does not execute properly. Try to figure out why.
+*/
+
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
