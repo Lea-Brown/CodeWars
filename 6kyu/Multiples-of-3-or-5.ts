a61deabc210f4065362c2eb802cf9dbba7fe46ca
Lea <leabrown198x@gmail.com>
2025-03-29 22:17:03 +0000

    codewars 29/3

---
+++ 6kyu/Multiples-of-3-or-5.ts
@@ -0,0 +1,23 @@
+/* 6 kyu
+Multiples of 3 or 5
+
+https://www.codewars.com/kata/514b92a657cdc65150000006/train/typescript
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Additionally, if the number is negative, return 0.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+*/
+
+export class Challenge {
+  static solution(number: number) {
+    let result = 0;
+    for (let i = 1; i < number; i++) {
+      if (i % 3 === 0 || i % 5 === 0) result += i;
+    }
+    return result;
+  }
+}
