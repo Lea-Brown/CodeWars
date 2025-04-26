565808c9659ed5e8bb9aba0ccbcf952d6088135a
Lea <leabrown198x@gmail.com>
2025-04-26 13:57:06 +0100

    codewars 26/4

---
+++ 6kyu/Roman-Numerals-Decoder.ts
@@ -0,0 +1,56 @@
+/* 6 kyu
+Roman Numerals Decoder
+
+https://www.codewars.com/kata/51b6249c4612257ac0000005/typescript
+
+Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+Example:
+"MM"      -> 2000
+"MDCLXVI" -> 1666
+"M"       -> 1000
+"CD"      ->  400
+"XC"      ->   90
+"XL"      ->   40
+"I"       ->    1
+
+Help:
+Symbol    Value
+I          1
+V          5
+X          10
+L          50
+C          100
+D          500
+M          1,000
+*/
+
+export function solution(roman: string): number {
+  const num: { [key: string]: number } = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000,
+  };
+  let count: number = 0;
+  for (let i = 0; i < roman.length; i++) {
+    if (num[roman[i] + roman[i + 1]]) {
+      count += num[roman[i] + roman[i + 1]];
+      i++;
+    } else {
+      count += num[roman[i]];
+    }
+  }
+  return count;
+}
