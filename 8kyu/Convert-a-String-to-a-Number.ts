6e355e0d5d9074e3aabfe9dee639d30eafe85259
Lea <leabrown198x@gmail.com>
2025-05-26 19:52:45 +0100

    codewars 26/5

---
+++ 8kyu/Convert-a-String-to-a-Number.ts
@@ -0,0 +1,19 @@
+/* 8 kyu
+Convert a String to a Number!
+
+https://www.codewars.com/kata/544675c6f971f7399a000e79/typescript
+
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+Examples
+"1234" --> 1234
+"605"  --> 605
+"1405" --> 1405
+"-7" --> -7
+*/
+
+export function stringToNumber(str: string): number {
+  return +str;
+}
