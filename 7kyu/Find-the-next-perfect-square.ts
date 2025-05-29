5e3aa083b22b5498104db5eeb77b4d13afc15d74
Lea <leabrown198x@gmail.com>
2025-05-29 15:35:40 +0100

    codewars 29/5

---
+++ 7kyu/Find-the-next-perfect-square.ts
@@ -0,0 +1,21 @@
+/* 7 kyu
+Find the next perfect square!
+
+https://www.codewars.com/kata/56269eb78ad2e4ced1000013/typescript
+
+You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+Examples ( Input --> Output )
+121 --> 144
+625 --> 676
+114 --> -1  #  because 114 is not a perfect square
+*/
+
+export function findNextSquare(sq: number): number {
+  if (!Number.isInteger(Math.sqrt(sq))) return -1;
+  return (Math.sqrt(sq) + 1) ** 2;
+}
