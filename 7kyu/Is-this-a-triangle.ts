6d5617a8c8b42874e9f5edc25cfb7f7800ce06c2
Lea <leabrown198x@gmail.com>
2025-06-10 12:39:15 +0100

    codewars 10/6

---
+++ 7kyu/Is-this-a-triangle.ts
@@ -0,0 +1,24 @@
+/* 7 kyu
+Is this a triangle?
+
+https://www.codewars.com/kata/56606694ec01347ce800001b/typescript
+
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+
+Examples:
+Input -> Output
+1,2,2 -> true
+4,2,3 -> true
+2,2,2 -> true
+1,2,3 -> false
+-5,1,3 -> false
+0,2,3 -> false
+1,2,9 -> false 
+*/
+
+export function isTriangle(a: number, b: number, c: number): boolean {
+  if (a + b <= c || a + c <= b || b + c <= a) return false;
+  return true;
+}
