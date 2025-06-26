ee2de659d2826cd931d4d7a4f4c2c552a9ab5a2f
Lea <leabrown198x@gmail.com>
2025-06-26 12:03:01 +0100

    codewars 26/6

---
+++ 8kyu/MakeUpperCase.ts
@@ -0,0 +1,11 @@
+/* 8 kyu
+MakeUpperCase
+
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/typescript
+
+Write a function which converts the input string to uppercase.
+*/
+
+export function makeUpperCase(str: string): string {
+  return str.toUpperCase();
+}
