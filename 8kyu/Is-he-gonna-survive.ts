e8b8f40443fee1fb28fe84ba7b3a74d481710a40
Lea <leabrown198x@gmail.com>
2025-07-04 18:56:30 +0100

    codewars 4/7

---
+++ 8kyu/Is-he-gonna-survive.ts
@@ -0,0 +1,13 @@
+/* 8 kyu
+Is he gonna survive?
+
+https://www.codewars.com/kata/59ca8246d751df55cc00014c/typescript
+
+A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return true if yes, false otherwise :)
+*/
+
+export function hero(bullets: number, dragons: number): boolean {
+  return bullets / 2 >= dragons;
+}
