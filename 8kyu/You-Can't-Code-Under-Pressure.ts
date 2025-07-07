17715e975244ced87e71fc397a7c96f01af583ab
Lea <leabrown198x@gmail.com>
2025-07-07 12:18:24 +0100

    codewars 7/7

---
+++ 8kyu/You-Can't-Code-Under-Pressure.ts
@@ -0,0 +1,11 @@
+/* 8 kyu
+You Can't Code Under Pressure #1
+
+https://www.codewars.com/kata/53ee5429ba190077850011d4/typescript
+
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+export function doubleInteger(i: number): number {
+  return i * 2;
+}
