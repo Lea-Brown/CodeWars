e31e370cf0e5a4239e57172d5267ccd00f0d592f
Lea <leabrown198x@gmail.com>
2025-08-10 19:44:06 +0100

    codewars 10/8

---
+++ 8kyu/Is-it-even.ts
@@ -0,0 +1,19 @@
+/* 8 kyu
+Is it even?
+
+https://www.codewars.com/kata/555a67db74814aa4ee0001b5/typescript
+
+In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+*/
+
+export const testEven = (n: number): boolean => {
+  return n % 2 === 0;
+};
