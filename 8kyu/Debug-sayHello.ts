97d7785f1a1cd2eb7de5e029311fe7db7b388cd0
Lea <leabrown198x@gmail.com>
2025-08-23 13:16:14 +0100

    codewars 23/8

---
+++ 8kyu/Debug-sayHello.ts
@@ -0,0 +1,16 @@
+/* 8 kyu
+Grasshopper - Debug sayHello
+
+https://www.codewars.com/kata/5625618b1fe21ab49f00001f/typescript
+
+Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+*/
+
+export function sayHello(name: string): string {
+  return `Hello, ${name}`;
+}
