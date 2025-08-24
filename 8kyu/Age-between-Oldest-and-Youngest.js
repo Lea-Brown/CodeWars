b3b1edf01970a1bfcc425365b91a4153889a4301
Lea <leabrown198x@gmail.com>
2025-08-24 22:13:09 +0100

    codewars 24/8

--- 8kyu/Age-between-Oldest-and-Youngest.js
+++ 8kyu/Age-between-Oldest-and-Youngest.js
@@ -8,7 +8,7 @@ At the annual family gathering, the family likes to find the oldest living famil
 You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
 */
 
-const differenceInAges = ages => [
+const differenceInAges = (ages) => [
   Math.min(...ages),
   Math.max(...ages),
   Math.max(...ages) - Math.min(...ages),
---
+++ 8kyu/Difference-of-Volumes-of-Cuboids.ts
@@ -0,0 +1,22 @@
+/* 8 kyu
+Difference of Volumes of Cuboids
+
+https://www.codewars.com/kata/58cb43f4256836ed95000f97/typescript
+
+In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+Your function will be tested with pre-made examples as well as random ones.
+
+If you can, try writing it in one line of code.
+*/
+
+export function findDifference(
+  a: [number, number, number],
+  b: [number, number, number]
+): number {
+  return Math.abs(
+    a.reduce((acc, add) => acc * add, 1) - b.reduce((acc, add) => acc * add, 1)
+  );
+}
