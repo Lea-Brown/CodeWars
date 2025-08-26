04346d1ad866f7adb9e106b148f3cf81d2314b86
Lea <leabrown198x@gmail.com>
2025-08-26 14:01:57 +0100

    codewars 26/8

---
+++ 8kyu/squaring-an-argument.ts
@@ -0,0 +1,9 @@
+/* 8 kyu
+Function 2 - squaring an argument
+
+https://www.codewars.com/kata/523b623152af8a30c6000027/typescript
+
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+export const square = (num: number): number => num ** 2;
