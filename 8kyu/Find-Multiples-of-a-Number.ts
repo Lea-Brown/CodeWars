607df0c70e07491216a1608f861aac083f860f09
Lea <leabrown198x@gmail.com>
2025-08-29 17:05:21 +0100

    codewars 29/8

---
+++ 8kyu/Find-Multiples-of-a-Number.ts
@@ -0,0 +1,23 @@
+/* 8 kyu
+Find Multiples of a Number
+
+https://www.codewars.com/kata/58ca658cc0d6401f2700045f/typescript
+
+In this simple exercise, you will write a function that takes two integers; n and limit; and returns a list of the multiples of n up to and possibly including limit.
+
+It is guaranteed that n > 0 and limit >= n.
+
+For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+
+Examples
+n = 2; limit = 6 --> [2, 4, 6]
+n = 2; limit = 5 --> [2, 4]
+*/
+
+export function findMultiples(integer: number, limit: number): number[] {
+  let result: number[] = [];
+  for (let i = integer; i <= limit; i += integer) {
+    result.push(i);
+  }
+  return result;
+}
