b7d10dfeca4a4b290faafad401e526c6086a3612
Lea <leabrown198x@gmail.com>
2025-08-30 16:54:00 +0100

    codewars 30/8

---
+++ 8kyu/If-else-syntax-debug.ts
@@ -0,0 +1,15 @@
+/* 8 kyu
+Grasshopper - If/else syntax debug
+
+https://www.codewars.com/kata/57089707fe2d01529f00024a/typescript
+
+While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function.
+
+checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
+
+The function receives one parameter health which will always be a whole number between -10 and 10.
+*/
+
+export function checkAlive(health: number): boolean {
+  return health > 0;
+}
