69740a414662a6df2ceba94aaad3a3ee17006ca8
Lea <leabrown198x@gmail.com>
2025-09-06 15:32:42 +0100

    codewars 6/9

---
+++ 8kyu/Basic-variable-assignment.ts
@@ -0,0 +1,9 @@
+/* 8 kyu
+Basic variable assignment
+
+https://www.codewars.com/kata/50ee6b0bdeab583673000025/typescript
+
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+*/
+
+export const name = "codewa.rs";
