f85ed4d2e777f51ab0f32c872536df5091b8a56d
Lea <leabrown198x@gmail.com>
2025-09-08 13:34:12 +0100

    codewars 8/9

---
+++ 8kyu/Basic-Function-Fixer.ts
@@ -0,0 +1,11 @@
+/* 8 kyu
+Grasshopper - Basic Function Fixer
+
+https://www.codewars.com/kata/56200d610758762fb0000002/typescript
+
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+*/
+
+export const addFive = (num: number): number => num + 5;
