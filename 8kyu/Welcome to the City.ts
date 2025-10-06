c16e06aa8496820e41da9018a178b14f81273ed2
Lea <leabrown198x@gmail.com>
2025-10-06 18:28:08 +0100

    codewars 6/10

---
+++ 8kyu/Welcome to the City.ts
@@ -0,0 +1,20 @@
+/* 8 kyu
+Welcome to the City
+
+https://www.codewars.com/kata/5302d846be2a9189af0001e4/typescript
+
+Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+Example:
+
+['John', 'Smith'], 'Phoenix', 'Arizona'
+This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+*/
+
+export const sayHello = (
+  name: string[],
+  city: string,
+  state: string
+): string => {
+  return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
+};
