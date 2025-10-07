4f1736752a0d0a4b2d468c452aab502e1a870bf8
Lea <leabrown198x@gmail.com>
2025-10-07 18:09:45 +0100

    codewars 7/10

---
+++ 8kyu/Price of Mangoes.ts
@@ -0,0 +1,40 @@
+/* 8 kyu
+Price of Mangoes
+
+https://www.codewars.com/kata/57a77726bb9944d000000b06/typescript
+
+Accountant time! For a given quantity and price (per mango), calculate the total cost of the mangoes.
+But! Every third mango is free!
+
+Examples
+# 🥭 : Paid mango
+# 🆓 : Free mango
+
+Quantity = 2
+Price = 3
+Total cost ==> 6    
+# Paid 2 mangoes for $3 per unit = $6; no mango for free
+# 🥭🥭
+
+Quantity = 3
+Price = 3
+Total cost ==> 6    
+# Paid 2 mangoes for $3 per unit = $6; +1 mango for free
+# 🥭🥭🆓
+
+Quantity = 5
+Price = 3
+Total cost ==> 12   
+# Paid 4 mangoes for $3 per unit = $12; +1 mango for free
+# 🥭🥭🆓   🥭🥭
+
+Quantity = 9
+Price = 5
+Total cost ==> 30   
+# Paid 6 mangoes for $5 per unit = $30; +3 mangoes for free
+# 🥭🥭🆓   🥭🥭🆓   🥭🥭🆓
+*/
+
+export function mango(quantity: number, price: number): number {
+  return (quantity - Math.floor(quantity / 3)) * price;
+}
