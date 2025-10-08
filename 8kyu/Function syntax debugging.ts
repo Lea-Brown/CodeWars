4b257b0de695f501b3d5c04a96b37d542d7de340
Lea <leabrown198x@gmail.com>
2025-10-08 18:57:30 +0100

    codewars 8/10

---
+++ 8kyu/Function syntax debugging.ts
@@ -0,0 +1,11 @@
+/* 8 kyu
+Grasshopper - Function syntax debugging
+
+https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/typescript
+
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+*/
+
+export function main(verb: string, noun: string): string {
+  return verb + noun;
+}
