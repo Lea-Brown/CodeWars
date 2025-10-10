b1a899ab12ca267025b029ed6d90bdd3e387856c
Lea <leabrown198x@gmail.com>
2025-10-10 18:32:38 +0100

    codewars 10/10

---
+++ 8kyu/Check same case.ts
@@ -0,0 +1,26 @@
+/* 8 kyu
+Check same case
+
+https://www.codewars.com/kata/5dd462a573ee6d0014ce715b/typescript
+
+Write a function that will check if two given characters are the same case.
+
+If either of the characters is not a letter, return -1
+If both characters are the same case, return 1
+If both characters are letters, but not the same case, return 0
+Examples
+'a' and 'g' returns 1
+
+'A' and 'C' returns 1
+
+'b' and 'G' returns 0
+
+'B' and 'g' returns 0
+
+'0' and '?' returns -1
+*/
+
+export function sameCase(a: string, b: string): number {
+  if (!/[a-z]/i.test(a) || !/[a-z]/i.test(b)) return -1;
+  return (a === a.toUpperCase()) === (b === b.toUpperCase()) ? 1 : 0;
+}
