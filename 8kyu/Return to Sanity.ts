e970c40a6abcf1026d655a0b1fc52f3895f7d50b
Lea <leabrown198x@gmail.com>
2025-10-14 18:41:16 +0100

    codewars 14/10

---
+++ 8kyu/Return to Sanity.ts
@@ -0,0 +1,12 @@
+/* 8 kyu
+Return to Sanity
+
+https://www.codewars.com/kata/514a7ac1a33775cbb500001e/typescript
+
+This function should return an object, but it's not doing what's intended. What's wrong?
+*/
+
+export function mystery(): object {
+  const results = { sanity: "Hello" };
+  return results;
+}
