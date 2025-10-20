244df5dcf9383780174f8e95ddbad77c450a9932
Lea <leabrown198x@gmail.com>
2025-10-20 17:12:41 +0100

    codewars 20/10

---
+++ 8kyu/Type of sum.ts
@@ -0,0 +1,11 @@
+/* 8 kyu
+Type of sum
+
+https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba/javascript
+
+Return the type of the sum of the two arguments
+*/
+
+function typeOfSum(a: any, b: any) {
+  return typeof (a + b);
+}
