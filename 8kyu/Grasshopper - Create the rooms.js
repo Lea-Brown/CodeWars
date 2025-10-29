3842013e2b19fa94ac6a6d7f450d7a3f2cf4e6aa
Lea <leabrown198x@gmail.com>
2025-10-29 17:57:59 +0000

    codewwars 29/10

---
+++ 8kyu/Grasshopper - Create the rooms.js
@@ -0,0 +1,34 @@
+/**
+ * 8 kyu
+ * Grasshopper - Create the rooms
+ * https://www.codewars.com/kata/56a29b237e9e997ff2000048/javascript
+ *
+ * Represents a collection of rooms, where each room has a name,
+ * description, and completion status.
+ *
+ * Each room object contains:
+ * - `name`: The room’s name.
+ * - `description`: A short description of the room.
+ * - `completed`: Indicates whether the room has been completed.
+ *
+ * @example
+ * rooms.room1.name; // "name"
+ * rooms.room2.completed; // "completed"
+ */
+let rooms = {
+  room1: {
+    name: "name",
+    description: "description",
+    completed: "completed",
+  },
+  room2: {
+    name: "name",
+    description: "description",
+    completed: "completed",
+  },
+  room3: {
+    name: "name",
+    description: "description",
+    completed: "completed",
+  },
+};
